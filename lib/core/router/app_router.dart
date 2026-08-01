9310be235e5728a6ded0a9a81fb007162d2fc57f
mofakkharul-29 <mofakkharulislam29@gmail.com>
2026-08-01 13:36:47 +0600

    router basic added

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  static final GlobalKey<NavigatorState> rootNavigatorKey =
+      GlobalKey<NavigatorState>();
+
+  static final Provider<GoRouter> appRouterProvider = Provider((ref) {
+    return GoRouter(
+      initialLocation: '/',
+      debugLogDiagnostics: true,
+      navigatorKey: rootNavigatorKey,
+      routes: [],
+    );
+  });
+}
