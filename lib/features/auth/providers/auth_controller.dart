269f43300df26e6fd519c6638ebee4901dc4b83f
mofakkharul-29 <mofakkharulislam29@gmail.com>
2026-08-27 01:14:35 +0600

    auth_controller done

--- lib/features/auth/providers/auth_controller.dart
+++ lib/features/auth/providers/auth_controller.dart
@@ -1,11 +1,63 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quick_bite/features/auth/providers/auth_repository_provider.dart';
 
-// class AuthController extends AsyncNotifier<Session?> {
-//   @override
-//   Future<Session?> build() {
-//     return 
-//   }
-// }
+class AuthController extends AsyncNotifier<void> {
+  @override
+  FutureOr<void> build() {}
+
+  Future<void> login({required String email, required String password}) async {
+    state = const AsyncLoading();
+
+    try {
+      final repo = ref.read(authRepositoryProvider);
+      await repo.signIn(email: email, password: password);
+      state = const AsyncData(null);
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
+  }
+
+  Future<void> register({
+    required String email,
+    required String password,
+    required String fullName,
+    String? phone,
+  }) async {
+    state = const AsyncLoading();
+    try {
+      await ref
+          .read(authRepositoryProvider)
+          .signUp(
+            email: email,
+            password: password,
+            fullName: fullName,
+            phone: phone,
+          );
+      state = const AsyncData(null);
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
+  }
+
+  Future<void> logout() async {
+    state = const AsyncLoading();
+    try {
+      await ref.read(authRepositoryProvider).signOut();
+      state = const AsyncData(null);
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
+  }
+
+  Future<void> deactivateAccount() async {
+    state = const AsyncLoading();
+    try {
+      await ref.read(authRepositoryProvider).deactivateAccount();
+      state = const AsyncData(null);
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
+  }
+}
